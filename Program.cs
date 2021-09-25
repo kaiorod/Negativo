b351d8edbb02ebfb3775e6f3b72f803c140b1af2
kaiorod <caiorodrigo2775@gmail.com>
2021-09-24 22:43:14 -0300

    Finaliza Negativo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+
+            Console.Beep();
+            Console.Clear();
+
+            Console.Write("Digite o número : ");
+            n  = double.Parse(Console.ReadLine());
+
+            if(n < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("O número é negativo!!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("O número é positivo!!");
+                Console.ResetColor();
+            }
+        }
+    }
+}
